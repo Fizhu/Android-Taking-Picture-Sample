9010a309edeb2fa28bf5afb2a06e1d1fa2b5d096
fizhu <hvyz.anbiya@gmail.com>
2021-12-23 16:36:17 +0700

    adding fab for taking pictures

--- app/src/main/java/com/example/takingpicturesample/MainActivity.kt
+++ app/src/main/java/com/example/takingpicturesample/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.takingpicturesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.takingpicturesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onInit()
     }
+
+    private fun onInit() {
+        binding.fabMain.setOnClickListener {
+            if (View.GONE == binding.frameBg.visibility) {
+                showFABMenu()
+            } else {
+                closeFABMenu()
+            }
+        }
+        binding.frameBg.setOnClickListener {
+            closeFABMenu()
+        }
+    }
+
+    private fun showFABMenu() {
+        binding.fabCamera.visible()
+        binding.fabGalery.visible()
+        binding.frameBg.visible()
+        binding.fabMain.animate().rotationBy(180F)
+        binding.fabCamera.animate().translationY(-resources.getDimension(R.dimen.standard_74))
+        binding.fabGalery.animate().translationY(-resources.getDimension(R.dimen.standard_128))
+    }
+
+    private fun closeFABMenu() {
+        binding.frameBg.gone()
+        binding.fabCamera.gone()
+        binding.fabGalery.gone()
+        binding.fabMain.animate().rotation(0F)
+        binding.fabCamera.animate().translationY(0f)
+        binding.fabGalery.animate().translationY(0f)
+    }
+
+    companion object {
+        fun View.gone() {
+            visibility = View.GONE
+        }
+
+        fun View.visible() {
+            visibility = View.VISIBLE
+        }
+    }
+
 }
